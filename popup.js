d4c226327aadfe4a456ef606529f6b0a17c836e7
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2026-06-07 13:05:59 -0700

    TimeMachine Chrome extension

---
+++ popup.js
@@ -0,0 +1,17 @@
+console.log("Popup script loaded"); // Console log scripts to see if ran.
+console.log("Time Machine is now Ready")
+
+const text = "Hello, World";
+const el = document.getElementById("typewriter"); // typewriter element fetched from popup
+let i = 0;
+let speed = 1.35; // speed of typing for Hello, World text
+
+function type() {
+    if (i < text.length) {
+        el.textContent += text[i];
+        i++;
+        setTimeout(type, 80/speed); // do not modify 80, instead modify speed variable.
+    }
+}
+
+type(); // runs function type.
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Time Machine
+
+Time Machine is a chrome extenstion built for navigating the Wayback Machine, all the way back!
+
+Built by GamerC0der
